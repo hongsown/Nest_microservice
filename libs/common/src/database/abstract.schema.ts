68f8c3ba391866cd5fc61b08390f44018b1a5a95
Sơn <88760761+ahihison@users.noreply.github.com>
2024-09-10 18:44:44 +0700

    feat: abstract repository db

---
+++ libs/common/src/database/abstract.schema.ts
@@ -0,0 +1,9 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+import { SchemaTypes, Types } from "mongoose";
+
+@Schema()
+
+export class AbstractSchema extends Document {
+  @Prop({ type: SchemaTypes.ObjectId })
+  _id: Types.ObjectId;
+}
